85f1ea8fb85efbf7bf96461c5a99a13d6c4895ce
alexmodesto <alex.modesto.7e5@itb.cat>
2024-06-05 01:14:59 +0200

    Initial commit

--- script.js
+++ script.js
@@ -2,17 +2,9 @@ document.getElementById('spinButton').addEventListener('click', function() {
     const audio = new Audio('spin-sound.mp3'); // Asegúrate de usar el nombre correcto del archivo de sonido
     audio.play();
 
-    fetch('noms.txt')
-        .then(response => response.text())
-        .then(data => {
-            const names = data.split('\n');
-            const randomIndex = Math.floor(Math.random() * names.length);
-            const nameContainer = document.getElementById('resultName');
-            nameContainer.textContent = names[randomIndex];
-            nameContainer.style.animation = 'spin 2s linear';
-            setTimeout(() => nameContainer.style.animation = 'none', 2000); // Elimina la animación después de ejecutarse
-        })
-        .catch(error => console.error('Error loading the names:', error));
+    let degrees = Math.floor(Math.random() * 360) + 1440; // Giro aleatorio + múltiples vueltas
+    const wheel = document.getElementById('wheel');
+    wheel.style.transform = `rotate(${degrees}deg)`;
 });
 
 document.getElementById('themeButton').addEventListener('click', function() {
